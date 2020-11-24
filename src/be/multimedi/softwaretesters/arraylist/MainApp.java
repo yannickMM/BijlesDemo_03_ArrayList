74f296c4a20d2a33a14166c85382399c2b2116a8
Yannick <vanham.yannick@gmail.com>
2020-11-24 10:28:49 +0100

    Add comments

--- src/be/multimedi/softwaretesters/arraylist/MainApp.java
+++ src/be/multimedi/softwaretesters/arraylist/MainApp.java
@@ -10,14 +10,21 @@ import java.util.Arrays;
 public class MainApp {
     public static void main(String[] args) {
 
+        // 03.01 ArrayList
+        // You can declare and initialise an ArrayList by using Arrays.asList
         ArrayList<String> guestList = new ArrayList<>(
                 Arrays.asList("Michael Shannon", "Henry Witherton", "Sara Sherwood", "Michaela Stone")
         );
 
         String weddingCrasher = "Jonathan Barnes";
 
+        // ArrayLists are mutable (have resizable lengths) so adding elements beyond the capacity is as easy
+        // as making use of an 'add' method
         guestList.add(weddingCrasher);
 
-        guestList.forEach();
+        // 03.02 Accessing ArrayLists
+        // Access ArrayList elements with 'get' and the element's index
+        // Note: ArraLists are zero-indexed, meaning you start counting from 0 not from 1
+        System.out.println(guestList.get(0));
     }
 }
